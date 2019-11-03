e6e6108f5542693d17dc15d60250d1d658c70732
Brooks Becton <bbecton@babelstreet.com>
2019-11-02 20:27:52 -0400

    Add strikethrough on press in IngredientList

--- src/components/IngredientList.tsx
+++ src/components/IngredientList.tsx
@@ -1,14 +1,40 @@
-import React from "react"
+import React, { useState } from "react"
 
 interface IProps {
   ingredients: string[]
 }
 
+function distinct(arr) {
+  return [...new Set(arr)]
+}
+
 export const IngredientList = ({ ingredients }: IProps) => {
+  const [selectedIds, setSelectedIds] = useState([])
+  const isSelected = index => selectedIds.indexOf(index) !== -1
+
   return (
     <ul>
-      {ingredients.map(ingredient => (
-        <li>{ingredient}</li>
+      {ingredients.map((ingredient, index) => (
+        <li
+          style={
+            isSelected(index)
+              ? {
+                  textDecoration: "line-through",
+                }
+              : {}
+          }
+          onClick={() =>
+            setSelectedIds(prevSelectedIds => {
+              if (isSelected(index)) {
+                return prevSelectedIds.filter(id => id !== index)
+              } else {
+                return distinct([...prevSelectedIds, index])
+              }
+            })
+          }
+        >
+          {ingredient}
+        </li>
       ))}
     </ul>
   )
