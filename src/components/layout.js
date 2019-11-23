95d2512d7ab13888dfbd91300216955eab441548
Brooks Becton <bbecton@babelstreet.com>
2019-11-23 14:56:24 -0500

    Make grid on home recipes

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,7 +27,7 @@ const Layout = ({ children }) => {
   const [isShowingSearch, setIsShowingSearch] = useState(false)
   return (
     <div>
-      <div className="mx-auto p-6 mb-12">
+      <div className="mx-auto p-6 md:pr-32 md:pl-32  mb-12">
         <Dialog
           aria-label="Search Site Form"
           isOpen={isShowingSearch}
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,73 +16,79 @@ class BlogIndex extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="All posts" />
 
-        <div>
-          <h2>Recent Recipes</h2>
-          <ul>
-            {posts.slice(0, 9).map(({ node }) => {
-              const title = node.frontmatter.title || node.fields.slug
-              return (
-                <li key={node.fields.slug}>
-                  <article className="mb-6 p-5 max-w-sm rounded overflow-hidden shadow-lg">
-                    <header>
-                      <p className="font-bold text-xl mb-2">
-                        <Link
-                          style={{ boxShadow: `none` }}
-                          to={node.fields.slug}
-                        >
-                          {title}
+        <div className="flex flex-col-reverse lg:flex-row">
+          <div className="lg:pr-12 lg:w-1/4">
+            <h2>Tags</h2>
+            <ul>
+              {tags.slice(0, 9).map(({ fieldValue, totalCount }) => {
+                const tag = fieldValue
+                const tagPath = `/tags/`
+                return (
+                  <li key={tagPath + tag}>
+                    <article>
+                      <header>
+                        <Link style={{ boxShadow: `none` }} to={`/`}>
+                          <Tag>
+                            #{tag} ({totalCount})
+                          </Tag>
                         </Link>
-                      </p>
-                    </header>
-                    <p className="text-gray-700 text-base">
-                      {node.excerpt.slice(19, node.excerpt.length)}
-                    </p>
-                    <div>
-                      {node.frontmatter.tags.map(tag => {
-                        return <Tag key={tag}>#{tag}</Tag>
-                      })}
-                    </div>
-                  </article>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
-        <div style={{ marginBottom: "60px" }}>
-          <h2>Tags</h2>
-          <ul>
-            {tags.slice(0, 9).map(({ fieldValue, totalCount }) => {
-              const tag = fieldValue
-              const tagPath = `/tags/`
-              return (
-                <li key={tagPath + tag}>
-                  <article>
-                    <header>
+                      </header>
+                    </article>
+                  </li>
+                )
+              })}
+              <li className="mt-3">
+                <article>
+                  <header>
+                    <p>
                       <Link
+                        className="text-xl"
                         style={{ boxShadow: `none` }}
-                        to={`${tagPath}/${tag}`}
+                        to="/tags"
                       >
-                        <Tag>
-                          #{tag} ({totalCount})
-                        </Tag>
+                        All Tags ({tags.length})
                       </Link>
-                    </header>
-                  </article>
-                </li>
-              )
-            })}
-            <li className='mt-3'>
-              <article>
-                <header>
-                  <p>
-                    <Link className='text-xl' style={{ boxShadow: `none` }} to="/tags">
-                      All Tags ({tags.length})
-                    </Link>
-                  </p>
-                </header>
-              </article>
-            </li>
-          </ul>
+                    </p>
+                  </header>
+                </article>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h2>Recent Recipes</h2>
+            <ul className="lg:flex lg:flex-row lg:w-full lg:flex-wrap ">
+              {posts.slice(0, 9).map(({ node }) => {
+                const title = node.frontmatter.title || node.fields.slug
+                return (
+                  <li className="lg:1/5 lg:p-5" key={node.fields.slug}>
+                    <article className="mb-6 p-5 max-w-sm rounded overflow-hidden shadow-lg">
+                      <header>
+                        <p className="font-bold text-xl mb-2">
+                          <Link
+                            style={{ boxShadow: `none` }}
+                            to={node.fields.slug}
+                          >
+                            {title}
+                          </Link>
+                        </p>
+                      </header>
+                      <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                        <p className="text-gray-700 text-base">
+                          {node.excerpt.slice(19, node.excerpt.length)}
+                        </p>
+                      </Link>
+                      <div>
+                        {node.frontmatter.tags.map(tag => {
+                          return <Tag key={tag}>#{tag}</Tag>
+                        })}
+                      </div>
+                    </article>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
         </div>
       </Layout>
     )
