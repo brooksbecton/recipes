238463495c2a69f0f350c4331880093e73dd9cdf
Brooks Becton <bbecton@babelstreet.com>
2020-02-22 21:59:47 -0500

    Start Savannah trip

--- src/pages/savannah.tsx
+++ src/pages/savannah.tsx
@@ -1,43 +1,81 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 import Img, { FluidObject } from "gatsby-image"
-import Slider from "react-slick"
-
-// import "slick-carousel/slick/slick.css"
-// import "slick-carousel/slick/slick-theme.css"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+
+import Slider from "react-slick";
+
+class SimpleSlider extends React.Component {
+  render() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    );
+  }
+}
 
 interface IProps {
   data: any
 }
 
 const Savannah = ({ data = [] }: IProps) => {
-  var settings = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  }
-  const images = data.images.edges.map(img => img.node.childImageSharp.fluid)
+  const images = data.images.edges.map(img => ({
+    img: img.node.childImageSharp.fluid,
+    fileName: img.node.name,
+  }))
   const [activeImage, setActiveImage] = React.useState(images[0])
-
+  const descriptions = {
+    1: "Talmadge Memorial Bridge",
+  }
+  console.log(images)
   return (
     <Layout>
       <SEO title="Savannah August 2019" />
       <div className="m-3">
         <h1>Savannah</h1>
-        <ul className="lg:flex lg:flex-row lg:w-full md:w-1/2 lg:flex-wrap ">
-          {images.map((img: FluidObject) => {
+        <ul className="lg:flex lg:flex-row lg:w-full md:w-1/2 lg:flex-wrap w-screen justify-center">
+          {images.map(img => {
             return (
-              <li className="max-w-sm rounded overflow-hidden shadow-lg p-3">
-                <Img fluid={img} alt="" />
+              <li
+                key={img.name}
+                className=" w-1/4 rounded overflow-hidden shadow-lg p-3"
+              >
+                <Img fluid={img.img} alt="" />
 
                 <div className="px-6 py-4">
-                  <div className="font-bold text-xl mb-2">
-                    The Coldest Sunset
-                  </div>
+                  <p className="font-bold text-xl mb-2">
+                    {descriptions[img.fileName]}
+                  </p>
                 </div>
               </li>
             )
@@ -58,6 +96,7 @@ export const query = graphql`
     ) {
       edges {
         node {
+          name
           childImageSharp {
             fluid(maxWidth: 1000) {
               ...GatsbyImageSharpFluid
