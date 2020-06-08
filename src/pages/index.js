488ff96fb57d2a4f9fb9b9138e006be495134033
Brooks Becton <bbecton@babelstreet.com>
2020-06-08 08:32:23 -0400

    Remove extra div

--- src/pages/index.js
+++ src/pages/index.js
@@ -60,93 +60,86 @@ class BlogIndex extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="All posts" />
 
-          <div className="flex flex-col-reverse lg:flex-row">
-            <div className="lg:pr-12 lg:w-1/3 md:w-1/2 ">
-              <h2>Tags</h2>
-              <ul>
-                {tags.slice(0, 9).map(({ fieldValue, totalCount }) => {
-                  const tag = fieldValue
-                  const tagPath = `/tags/`
-                  return (
-                    <li className="m-5" key={tagPath + tag}>
-                      <article>
-                        <header>
+        <div className="flex flex-col-reverse lg:flex-row">
+          <div className="lg:pr-12 lg:w-1/3 md:w-1/2 ">
+            <h2>Tags</h2>
+            <ul>
+              {tags.slice(0, 9).map(({ fieldValue, totalCount }) => {
+                const tag = fieldValue
+                const tagPath = `/tags/`
+                return (
+                  <li className="m-5" key={tagPath + tag}>
+                    <article>
+                      <header>
+                        <Link style={{ boxShadow: `none` }} to={`/tags/${tag}`}>
+                          <Tag>
+                            {tag} ({totalCount})
+                          </Tag>
+                        </Link>
+                      </header>
+                    </article>
+                  </li>
+                )
+              })}
+              <li className="mt-3">
+                <article>
+                  <header>
+                    <p>
+                      <Link
+                        className="text-xl"
+                        style={{ boxShadow: `none` }}
+                        to="/tags"
+                      >
+                        All Tags ({tags.length})
+                      </Link>
+                    </p>
+                  </header>
+                </article>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h2>Recent Recipes</h2>
+            <ul className="lg:flex lg:flex-row lg:w-full md:w-1/2 lg:flex-wrap">
+              {posts.slice(0, 9).map(({ node }) => {
+                const title = node.frontmatter.title || node.fields.slug
+                return (
+                  <li className="lg:1/5 lg:p-5" key={node.fields.slug}>
+                    <article className="mb-6 p-5 max-w-sm rounded overflow-hidden hover:shadow-2xl shadow-lg">
+                      <header>
+                        <p className="font-bold text-xl mb-2">
                           <Link
                             style={{ boxShadow: `none` }}
-                            to={`/tags/${tag}`}
+                            to={node.fields.slug}
                           >
-                            <Tag>
-                              {tag} ({totalCount})
-                            </Tag>
+                            {title}
                           </Link>
-                        </header>
-                      </article>
-                    </li>
-                  )
-                })}
-                <li className="mt-3">
-                  <article>
-                    <header>
-                      <p>
-                        <Link
-                          className="text-xl"
-                          style={{ boxShadow: `none` }}
-                          to="/tags"
-                        >
-                          All Tags ({tags.length})
-                        </Link>
-                      </p>
-                    </header>
-                  </article>
-                </li>
-              </ul>
-            </div>
-
-            <div>
-              <h2>Recent Recipes</h2>
-              <ul className="lg:flex lg:flex-row lg:w-full md:w-1/2 lg:flex-wrap">
-                {posts.slice(0, 9).map(({ node }) => {
-                  const title = node.frontmatter.title || node.fields.slug
-                  return (
-                    <li className="lg:1/5 lg:p-5" key={node.fields.slug}>
-                      <article className="mb-6 p-5 max-w-sm rounded overflow-hidden hover:shadow-2xl shadow-lg">
-                        <header>
-                          <p className="font-bold text-xl mb-2">
+                        </p>
+                      </header>
+                      <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                        <p className="text-gray-700 text-base">
+                          {node.excerpt.slice(19, node.excerpt.length)}
+                        </p>
+                      </Link>
+                      <div>
+                        {node.frontmatter.tags.map(tag => {
+                          return (
                             <Link
-                              style={{ boxShadow: `none` }}
-                              to={node.fields.slug}
+                              style={{ boxShadow: `none`, marginTop: "10px" }}
+                              to={`/tags/${tag}`}
+                              key={tag}
                             >
-                              {title}
+                              <Tag key={tag}>{tag}</Tag>
                             </Link>
-                          </p>
-                        </header>
-                        <Link
-                          style={{ boxShadow: `none` }}
-                          to={node.fields.slug}
-                        >
-                          <p className="text-gray-700 text-base">
-                            {node.excerpt.slice(19, node.excerpt.length)}
-                          </p>
-                        </Link>
-                        <div>
-                          {node.frontmatter.tags.map(tag => {
-                            return (
-                              <Link
-                                style={{ boxShadow: `none`, marginTop: "10px" }}
-                                to={`/tags/${tag}`}
-                                key={tag}
-                              >
-                                <Tag key={tag}>{tag}</Tag>
-                              </Link>
-                            )
-                          })}
-                        </div>
-                      </article>
-                    </li>
-                  )
-                })}
-              </ul>
-            </div>
+                          )
+                        })}
+                      </div>
+                    </article>
+                  </li>
+                )
+              })}
+            </ul>
           </div>
         </div>
       </Layout>
